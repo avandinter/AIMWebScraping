1d3f9261cc36cc1571cbbc2d1cef01368ba4ac36
Alan Van Dinter <avand963@gmail.com>
2019-07-14 11:03:40 -0500

    scrapy demo work

--- AIMWebScraping/AIMWebScraping/library_demos/scrapy_demo.py
+++ AIMWebScraping/AIMWebScraping/library_demos/scrapy_demo.py
@@ -1,8 +1,12 @@
 import scrapy
 from scrapy.crawler import CrawlerRunner, CrawlerProcess
+import scrapy
+from scrapy.http import HtmlResponse
+from scrapy.selector import Selector
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 import time
+import json 
 
 class TableItem(scrapy.Item):
         table = scrapy.Field()
@@ -12,17 +16,14 @@ class TableItem(scrapy.Item):
         items = scrapy.Field()
         amount = scrapy.Field()
 
-class LegoSetPipeline(object):
+class SaverPipeline(object):
     def open_spider(self, spider):
-        print("open spider")
         timestr = time.strftime("%Y%m%d-%H%M%S")
         self.start_time = time.time()
         self.item_count = 0
-        self.file = open('AIMWebScraping/data/{}{}{}{}.json'.format( "products", spider.running_year, "_",timestr), 'w')
+        self.file = open('AIMWebScraping/data/{}{}{}.json'.format( "products", "_",timestr), 'w')
 
     def process_item(self, item, spider):
-        print("Process Item")
-        # This is normally where you would be running your post extraction logic and save to a database
         line = json.dumps(dict(item)) + "\n"
         self.file.write(line)
         self.item_count += 1
@@ -34,39 +35,49 @@ class LegoSetPipeline(object):
 
 class DemoSpider(scrapy.Spider):
     name = "crawler"
-    allowed_domains = ["brickset.com"]
-    running_year = ""
+    allowed_domains = ["testing-ground.scraping.pro"]
 
     def parse(self, response):
         params = response.url.split("?")[1]
 
         table = response.xpath("//div[@id='case_table']/table")[0]
+        header_row = table.xpath("thead/tr")[0]
 
         product_lookup = {}
         current_index = 0
-        for header in table.xpath("th"):
-            colspan = header.xpath("@colspan").get()
-            text = header.text
+        for header in header_row.xpath("th"):
+            colspan = header.xpath("@colspan").extract_first()
+            text = header.xpath("text()").extract_first()
             if colspan is None:
                 colspan = 1
             
-            for num in range(1,colspan, 1):
+            for num in range(0, int(colspan), 1):
                 current_index += 1
                 product_lookup[current_index] = text
-                print(text)
 
 
 
-        for row in table.xpath('tr'):
+        current_year = ""
+        for row in table.xpath('tbody/tr'):
             current_cell_index = 0
             quarter = ''
             for cell in row.xpath("td"):
-                colspan = header.xpath("@colspan").get()
-                text = header.xpath('text()').get()
+                colspan = cell.xpath("@colspan").extract_first()
+                text = cell.xpath('text()').extract_first()
+
+                if text is None:
+                    text = cell.xpath('center/text()').extract_first()
+
+                if text is None or "Toatal for" in text:
+                    break
+
                 if colspan is None:
                     colspan = 1
-                current_cell_index += colspan
-                    
+                current_cell_index += int(colspan)
+
+                if (int(colspan) == len(product_lookup)):
+                    current_year = text
+                    break
 
                 header = product_lookup[current_cell_index]
 
@@ -75,11 +86,11 @@ class DemoSpider(scrapy.Spider):
                     continue
                 elif header == "Total Amount":
                     continue
-                
-                
+
                 item = TableItem()
-                item.quarter = quarter
-                item.product = header
+                item["quarter"] = quarter
+                item["product"] = header
+                item["year"] = current_year
 
                 yield item
 
@@ -97,7 +108,7 @@ class ScrapyDemo(object):
 
         process = CrawlerProcess({
         'LOG_LEVEL': 'ERROR',
-        'ITEM_PIPELINES': { 'AIMWebScraping.Demos.webscraping_demo.LegoSetPipeline': 100 }
+        'ITEM_PIPELINES': { 'AIMWebScraping.library_demos.scrapy_demo.SaverPipeline': 100 }
         })
         for anchor in all_anchors:
             process.crawl(DemoSpider(), link = "http://testing-ground.scraping.pro" + anchor['href'])
